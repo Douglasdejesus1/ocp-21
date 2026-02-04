c30f80c82a818f0a6e6aa2d2b3feb8ddefc76e45
Douglas de Jesus <douglas.dj1242@gmail.com>
2026-02-03 22:17:51 -0300

    add new Java class IfClass for chapter03 with examples of if statements and instanceof operator

---
+++ src/chapter03/IfClass.java
@@ -0,0 +1,30 @@
+package chapter03;
+
+public class IfClass {
+    public static void main(String[] args) {
+
+        int a = 10;
+        if (a > 5) {
+            int b = 5;
+            System.out.println("true" + b);
+        }
+            // System.out.println("false" + b);
+
+
+    }
+
+    class InstanceOfClas {
+        public static void main(String[] args) {
+            Number meuNumero = 20;
+            if (!(meuNumero instanceof Number num)) {
+                System.out.println("nao é Number");
+                return;
+            }
+            System.out.println(num);
+            int b = num.intValue() + 10;
+            System.out.println(b);
+            if (b > 10)
+                System.out.println("b é maior que 10");
+        }
+    }
+}
